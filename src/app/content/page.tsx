5a62bd7e54a6611695a6bb60e6214158d167778e
KoreanJoey <dolha4166@gmail.com>
2025-06-10 16:15:35 +1000

    Make title and content state to manage form of post

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -1,16 +1,26 @@
+'use client'
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function CreateContent() {
-  const title = "";
-  const content = "";
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const handleTitle = ({e}: any) => {
+      setTitle(e.target.value)
+  }
+  const handleContent = ({e}: any) => {
+    setContent(e.target.value)
+}
+
   return (
     <>
       <form action="" className="p-4">
         <div className="flex flex-col gap-2">
           <span>Title</span>
-          <input type="text" className="w-44 border-2" />
+          <input type="text" className="w-44 border-2" onChange={handleTitle} value={title}/>
           <span>Content</span>
-          <textarea name="" id="" className="w-128 border-2"></textarea>
+          <textarea name="" id="" className="w-128 border-2" onChange={handleContent} value={content} ></textarea>
           <div className="flex flex-row gap-2 items-center">
             <Link href={"/"} className="w-20 bg-gray-600 rounded-sm">
               Cancel
