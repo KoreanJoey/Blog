590ba3161314ec78b16ebe3f9aa5d1ca0fb642af
KoreanJoey <dolha4166@gmail.com>
2025-06-16 16:20:42 +1000

    Add SignIn & SignUp page ui

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+
+import { useRouter } from "next/navigation";
+
+export default function SignUp() {
+
+    const router = useRouter();
+
+
+    return (<>
+    <h1>Sign Up</h1>
+    <div className="flex flex-col">
+        <span>Name</span>
+        <input type="text" className="border-2"></input>
+        <span>Email</span>
+        <input type="text" className="border-2"></input>
+        <span>Password</span>
+        <input type="text" className="border-2"></input>
+    </div>
+
+    <button onClick={() => {router.push("/")}} className="border-2 bg-gray-600 p-2 mt-4">
+        Sign Up
+    </button>
+    </>);
+};
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,24 @@
-'use client'
+"use client";
 
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Header() {
-    const [title, setTitle] = useState("Blog")
-    const [author, setAuthor] = useState("Joey")
+  const [title, setTitle] = useState("Blog");
+  const [author, setAuthor] = useState("Joey");
 
-    return (<>
-    <div className="flex flex-row gap-4 w-full my-10 mx-4 bg-gray-700">
-       <h1>{title}</h1>
-       <h2>{author}</h2>
-    </div>
-    </>);
-};
+  return (
+    <>
+      <div className="flex flex-row gap-4 my-10 mx-4 bg-gray-700 justify-between p-2">
+        <div className="flex flex-row gap-2">
+          <h1>{title}</h1>
+          <h2>{author}</h2>
+        </div>
+
+        <div>
+          <Link href={"/auth/sign-in"}>Sign In</Link> / <Link href={"/auth/sign-up"}>Sign Up</Link>
+        </div>
+      </div>
+    </>
+  );
+}
